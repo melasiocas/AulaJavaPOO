f1bd440e79fc6dc2dd04b2a59c288100079871fb
Melissa Lourenço de Souza <melasioca@gmail.com>
2024-12-08 20:18:43 -0300

    08/12/24

---
+++ src/main/java/com/senai/aula01_classe_atributo_metodos/exercicios/ex1/produto.java
@@ -0,0 +1,13 @@
+package com.senai.aula01_classe_atributo_metodos.exercicios.ex1;
+
+public class produto {
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    public produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+}
