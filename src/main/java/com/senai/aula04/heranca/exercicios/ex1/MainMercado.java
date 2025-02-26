1e020fd43b5c7016b71360178a15209c6324c34f
Melissa Lourenço de Souza <melasioca@gmail.com>
2025-02-26 09:03:40 -0300

    26/02/2025

--- src/main/java/com/senai/aula04/heranca/exercicios/ex1/MainMercado.java
+++ src/main/java/com/senai/aula04/heranca/exercicios/ex1/MainMercado.java
@@ -4,34 +4,67 @@ import static com.senai.aula03.encapsulamento.exercicios.ex2.Main.scanner;
 
 public class MainMercado {
     public static void main(String[] args) {
-        System.out.println("Bem Vindo ao Main Mercado!");
-       menu();
+        menu();
     }
-    public static void menu(){
-            System.out.println("""
-                    1 - Exibir Produtos existentes.
-                    2 - Mostrar Quantidade no Estoque.
-                    3 - Mostrar Valor total de Estoque.
-                    5 - Mudar Quantidade do Estoque.
-                    6 - Adicionar novos produtos.
-                    6 - Sair.
-                    """);
-            int opcao = scanner.nextInt();
-            scanner.nextLine();
-        do{
 
-            switch (opcao){
+    public static void menu() {
+        int opcaoInt;
+        do {
+           System.out.print("""
+                   1 - Exibir Produtos existentes.
+                   2 - Mostrar Quantidade no Estoque.
+                   3 - Mostrar Valor total de Estoque.
+                   5 - Mudar Quantidade do Estoque.
+                   5 - Adicionar novos produtos.
+                   6 - Sair.
+                   """);
+           String opcao = scanner.nextLine();
+           opcaoInt = castingStringForNumber(opcao);
+
+        } while (!(opcaoInt >= 1 && opcaoInt <= 6));
+
+            switch (opcaoInt) {
                 case 1:
                     menu();
+                    break;
                 case 2:
                     menu();
+                    break;
                 case 3:
                     menu();
+                    break;
                 case 4:
                     menu();
+                    break;
                 case 5:
+                    menu();
+                    break;
+                case 6:
                     break;
             }
-        } while (opcao != 5);
+        }
+
+    public static boolean InputCheck (String input){
+    try{
+        Integer.parseInt(input);
+        return true;
+    } catch (NumberFormatException e){
+        return false;
     }
+    }
+
+    public static int castingStringForNumber(String input) {
+        if (InputCheck(input) == true) {
+            int inputInt = Integer.parseInt(input);
+                return inputInt;
+        } else {
+            do {
+                System.out.println("O valor inserido não é um número válido. Digite o valor novamente: ");
+                input = scanner.nextLine();
+            } while (InputCheck(input) == false);
+            return Integer.parseInt(input);
+        }
+    }
+
+
 }
